1081616759d5b7fcc2b0cf33b6efbbea90be8bc2
Richard Mortier <mort@cantab.net>
2024-04-16 16:04:42 +0100

    research: update and format publications

--- templates/shortcodes/render_publications.py
+++ templates/shortcodes/render_publications.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import glob
+import json
 import logging
-import sys
+import os
 from typing import Optional
 
 import bibtexparser
@@ -11,11 +11,11 @@ from rich.console import Console
 from rich.logging import RichHandler
 from yattag import Doc, indent
 
-STRINGS = r"/home/mort/u/me/publications/strings.bib"
+HOMEPAGES = {}
 
 errcon = Console(stderr=True)
 logging.basicConfig(
-    level="INFO",
+    level="DEBUG",
     format="%(message)s",
     datefmt="[%X]",
     handlers=[RichHandler(console=errcon, rich_tracebacks=True)],
@@ -44,32 +44,39 @@ def html(bib, doc: Optional[Doc] = None) -> Doc:
 
     elif isinstance(bib, bibtexparser.model.Entry):
         try:
+            log.debug(f"{bib=}")
             authors = bib.get("author").value
             with doc.tag("span", klass="authors"):
                 for i, author in enumerate(authors):
-                    if i + 1 == len(authors):
+                    if i == 0:
+                        pass
+                    elif i + 1 == len(authors):
                         doc.text(" and ")
-
-                    klass = "author"
-                    if (author.first[0], author.last[0]) == ("Richard", "Mortier"):
-                        klass += " highlight"
-                    author.first = [f"{author.first[0][0].strip()}."]
-                    with doc.tag("span", klass=klass):
-                        doc.text(f"{author.merge_last_name_first}")
-                    if i + 1 < len(authors):
+                    else:
                         doc.text(", ")
 
-            doc.stag("br")
+                    klass = "author"
+                    key = f"{author.first[0]} {author.last[0]}"
+                    homepage = HOMEPAGES.get(key)
 
-            title = bib.get("title").value
-            with doc.tag("span", klass="title"):
-                doc.text(f"{title}")
+                    if (author.first[0], author.last[0]) == ("Richard", "Mortier"):
+                        klass += " highlight"
 
-            doc.stag("br")
+                    author.first = [f"{author.first[0][0].strip()}."]
+                    with doc.tag("span", klass=klass):
+                        if homepage:
+                            with doc.tag("a", href=homepage):
+                                doc.text(f"{author.merge_last_name_first}")
+                        else:
+                            doc.text(f"{author.merge_last_name_first}")
+
+            # compute date
 
             date = bib.get("issue_date")
             if date:
                 date = date.value
+                month, year = date.split(" ")
+
             else:
                 date = bib.get("date")
                 if date:
@@ -80,25 +87,113 @@ def html(bib, doc: Optional[Doc] = None) -> Doc:
                     month = bib.get("month").value if bib.get("month") else ""
                     month, *day = month.split("#")
                     day = " ".join(day if day else "").strip()
-                    month = {
-                        "": "",
-                        "jan": "January",
-                        "feb": "February",
-                        "mar": "March",
-                        "apr": "April",
-                        "may": "May",
-                        "jun": "June",
-                        "jul": "July",
-                        "aug": "August",
-                        "sep": "September",
-                        "oct": "October",
-                        "nov": "November",
-                        "dec": "December",
-                    }[month.strip()]
 
-                date = " ".join([day, month, year])
+                month = {
+                    "": "",
+                    "01": "January",
+                    "jan": "January",
+                    "02": "Feburary",
+                    "feb": "February",
+                    "03": "March",
+                    "mar": "March",
+                    "04": "April",
+                    "apr": "April",
+                    "05": "May",
+                    "may": "May",
+                    "06": "June",
+                    "jun": "June",
+                    "07": "July",
+                    "jul": "July",
+                    "08": "August",
+                    "aug": "August",
+                    "09": "September",
+                    "sep": "September",
+                    "10": "October",
+                    "oct": "October",
+                    "11": "November",
+                    "nov": "November",
+                    "12": "December",
+                    "dec": "December",
+                }[month.strip()]
+
+                date = f"{day} {month}, {year}"
+
+            # insert year
+            with doc.tag("span", klass="year"):
+                doc.text(f" ({year}) ")
+
+            # insert title
+            title = bib.get("title").value
+            with doc.tag("span", klass="title"):
+                doc.asis(f"&ldquo;{title}&rdquo;. ")
+
+            # insert venue
+            doc.text("In ")
+            addendum = ""
+            match bib.entry_type:
+                case "inproceedings":
+                    venue = bib["booktitle"]
+
+                    address = bib.get("address")
+                    if address:
+                        addendum += f". {address.value}"
+
+                    pages = bib.get("pages")
+                    if pages:
+                        addendum += f". pp.&nbsp;{pages.value}"
+
+                case "article":
+                    venue = bib.get("journaltitle")
+                    if not venue:
+                        venue = bib.get("journal")
+                    venue = venue.value
+
+                    volume = bib.get("volume")
+                    if volume:
+                        addendum += f"&nbsp;{volume.value}"
+
+                    number = bib.get("number")
+                    if number:
+                        addendum += f"({number.value})"
+
+                    pages = bib.get("pages")
+                    if pages:
+                        addendum += f":{pages.value}"
+
+                case "patent":
+                    venue = "PATENT"
+
+                case _:
+                    venue = "UNKNOWN VENUE"
+
+            with doc.tag("span", klass="venue"):
+                doc.asis(f"{venue.strip()}")
+
+            doc.asis(f"{addendum}. ")
+
+            # insert date
             with doc.tag("span", klass="date"):
-                doc.text(f"{date.strip()}")
+                doc.text(f"{date}. ")
+
+            # insert note
+            with doc.tag("span", klass="note"):
+                note = bib.get("note")
+                if note:
+                    doc.text(f"{note.value}. ".replace("..", "."))
+
+            # insert DOI
+            with doc.tag("span", klass="doi"):
+                doi = bib.get("doi")
+                if doi:
+                    with doc.tag("a", href=doi.value):
+                        doc.asis(f"doi:{doi.value} ")
+
+            # insert URL
+            with doc.tag("span", klass="url"):
+                url = bib.get("url")
+                if url:
+                    with doc.tag("a", href=url.value):
+                        doc.asis(f"{url.value}")
 
         except Exception:
             log.exception(f"ENTRY : {bib}")
@@ -106,8 +201,7 @@ def html(bib, doc: Optional[Doc] = None) -> Doc:
             # errcon.print_exception(show_locals=True)
 
     elif isinstance(bib, bibtexparser.library.Library):
-        with doc.tag("ul"):
-            html(bib.entries, doc)
+        html(bib.entries, doc)
         return doc
 
     else:
@@ -115,45 +209,59 @@ def html(bib, doc: Optional[Doc] = None) -> Doc:
 
 
 if __name__ == "__main__":
-    INPUT = sys.argv[1]
+    bibfiles = {
+        "Peer-reviewed journal papers": ["rmm-journal.bib"],
+        "Peer-reviewed conference papers": ["rmm-conference.bib"],
+        "Peer-reviewed workshop papers": ["rmm-workshop.bib"],
+        "Technical reports, editorials, book chapters, patents, etc.": [
+            "rmm-techreport.bib",
+            "rmm-patent.bib",
+            "rmm-unpublished.bib",
+        ],
+    }
 
-    bibfiles = glob.glob(INPUT)
+    with open(r"./authors.json") as f:
+        HOMEPAGES = json.load(f)
 
-    with open(STRINGS) as f:
+    with open(r"/home/mort/u/me/publications/strings.bib") as f:
         strings = f.read()
 
-    content = ""
-
-    for filename in bibfiles:
-        with open(filename) as f:
-            content += f.read()
-
-    library = bibtexparser.parse_string(
-        strings + content,
-        append_middleware=[
-            mw.LatexDecodingMiddleware(),
-            # mw.MonthIntMiddleware(),
-            mw.SeparateCoAuthors(),
-            mw.SplitNameParts(),
-            mw.SortBlocksByTypeAndKeyMiddleware(),
-            # mw.AddEnclosingMiddleware(),
-        ],
-    )
-
-    stats = {
-        "blocks": len(library.blocks),
-        "entries": len(library.entries),
-        "comments": len(library.comments),
-        "strings": len(library.strings),
-        "preamble": len(library.preambles),
-        "failures": len(library.failed_blocks),
-    }
-    log.info(f"STATS: f{stats=}")
-
-    if len(library.failed_blocks) > 0:
-        for failure in library.failed_blocks:
-            print(f"{failure.start_line}({failure.duplicate_keys}) {failure.raw}")
-
-    doc = html(library)
     print("{{dummy}}")
-    print(indent(doc.getvalue()))
+
+    for section, filenames in bibfiles.items():
+        content = ""
+        for filename in filenames:
+            with open(os.path.join("/home/mort/u/me/publications", filename)) as f:
+                content += f.read()
+
+        library = bibtexparser.parse_string(
+            strings + content,
+            parse_stack=[
+                mw.LatexDecodingMiddleware(),
+                mw.SeparateCoAuthors(),
+                mw.SplitNameParts(),
+                mw.SortFieldsCustomMiddleware(order=("year", "authors")),
+            ],
+        )
+
+        stats = {
+            "blocks": len(library.blocks),
+            "entries": len(library.entries),
+            "comments": len(library.comments),
+            "strings": len(library.strings),
+            "preamble": len(library.preambles),
+            "failures": len(library.failed_blocks),
+        }
+        log.info(f"STATS: f{stats=}")
+
+        if len(library.failed_blocks) > 0:
+            for failure in library.failed_blocks:
+                print(f"{failure.start_line}({failure.duplicate_keys}) {failure.raw}")
+
+        doc = Doc()
+        with doc.tag("section", klass="papers"):
+            doc.text(section)
+        with doc.tag("ol", klass="papers"):
+            html(library, doc)
+
+        print(indent(doc.getvalue()))
